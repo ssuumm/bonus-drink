7739851cb2aa7c40bcaeece068c1a69cfb64eb54
user <ryoc13@gmail.com>
2019-02-21 13:59:24 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,22 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount == 0 then
+      0
+    elsif amount > 0 && amount < 3
+      amount
+
+    elsif amount >= 3
+      bottle = amount / 3
+      extra = amount % 3
+      amount = amount + bottle
+      for amount in extra do
+        amount = amount + 1
+        if amount % 3 == 0 then
+          amount = amount + 1
+        end
+      end
+      amount
+
+    end
   end
 end
